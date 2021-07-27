920ca579a23891b9dbadb385449f9ad75d6266a4
Alejandro Cuevas <iscg@protonmail.com>
2021-07-26 22:55:21 -0700

    Remove unnecessary line

--- app/src/main/java/KobolAdventures/app/Main.java
+++ app/src/main/java/KobolAdventures/app/Main.java
@@ -8,7 +8,6 @@ public class Main
  
 	public static void main(String[] args)
 	{
-		SharedLibraryLoader.load()
 		System.out.println("start");
 		
 		GameTime GlobalGameTime = GameTime.getSystemTime();
