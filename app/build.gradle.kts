7398c0ab74ecc1d80ea217c004b0964d86736798
Alejandro Cuevas <iscg@protonmail.com>
2021-07-26 22:32:52 -0700

    Remove bracket

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,10 +9,10 @@ plugins {
 dependencies {
     implementation("org.apache.commons:commons-text")
     val lwjglVersion = "2.7.1"
-    implementation("org.lwjgl:lwjgl:$lwjglVersion}")
-    implementation("org.lwjgl:lwjgl-platform:$lwjglVersion}:natives-windows")
-    implementation("org.lwjgl:lwjgl-platform:$lwjglVersion}:natives-linux")
-    implementation("org.lwjgl:lwjgl-platform:$lwjglVersion}:natives-osx")
+    implementation("org.lwjgl:lwjgl:$lwjglVersion")
+    implementation("org.lwjgl:lwjgl-platform:$lwjglVersion:natives-windows")
+    implementation("org.lwjgl:lwjgl-platform:$lwjglVersion:natives-linux")
+    implementation("org.lwjgl:lwjgl-platform:$lwjglVersion:natives-osx")
 }
 
 application {
